505cbb22cfa913d4b8d38eb8579cc9e8882d0d84
AhmadAlMaradni <ahmad.maradni@hotmail.com>
2017-01-07 09:49:26 +0200

    stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,16 @@
 var Stack = function() {
-	//your code is here
+  var _storage = {}
+  var size= 0
+  _storage.add = function (value) {
+    _storage[size++]=value
+  }
+
+  _storage.remove = function () {
+    var temp = _storage[size-1]
+    delete _storage[size-1]
+    size--;
+    return temp
+
+  }
+  return _storage
 };
