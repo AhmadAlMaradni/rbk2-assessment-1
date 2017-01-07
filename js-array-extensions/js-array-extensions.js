7e1a401facddbdd34b0c7f18904efb3ddc171938
AhmadAlMaradni <ahmad.maradni@hotmail.com>
2017-01-07 10:33:48 +0200

    all

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,3 +1,22 @@
-Array.prototype.firt = function  {
-	return Array[0]
+Array.prototype.firts = function()  {
+  //var myArr = new firts()
+  //return new Array().this[0]
+ // var first = new Array()
+  return this.Array[0]
 }
+
+Array.prototype.last = function()  {
+  return Array[this.length-1]
+}
+
+// class getfirst extends Array {
+//   first (){
+//     return this[0]
+//   }
+// }
+
+// class getlast extends Array {
+//   getlast (){
+//     return this[0]
+//   }
+// }
